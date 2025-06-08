15905ae91bb6fc9ebb6962c3384ca09c11ca0589
EduardoRpo <proyectosrestrepowp@gmail.com>
2025-06-07 21:04:18 -0500

    archivo query

--- querys.sql
+++ querys.sql
@@ -29,3 +29,43 @@ FROM
     TRADE 
 WHERE  
     NRODCTO LIKE '14662' AND ORIGEN='COM' AND TIPODCTO='FS'
+
+    -----------------------------
+    select trade.nrodcto, trade.bruto, trade.IVABRUTO, trade.NIT, mtprocli.nombre, mvtrade.CANTIDAD, mvtrade.nombre as nombre_referencia, mvtrade.producto as numero_referencia 
+from trade 
+inner join mvtrade on trade.nrodcto=mvtrade.nrodcto
+inner join mtprocli on trade.nit=mtprocli.nit
+
+where trade.nrodcto='11164' and trade.ORIGEN='COM' and trade.tipodcto='OC' and mvtrade.ORIGEN='COM' and mvtrade.tipodcto='OC' 
+
+--------------------
+-- Paso 1: Agregar las columnas con valores predeterminados
+ALTER TABLE facturas
+ADD COLUMN nrodcto_oc VARCHAR(50) NOT NULL DEFAULT 'DEFAULT_NRODCTO',
+ADD COLUMN bruto_oc DECIMAL(15, 2),
+ADD COLUMN ivabruto_oc DECIMAL(15, 2),
+ADD COLUMN nit_oc VARCHAR(20) NOT NULL DEFAULT 'DEFAULT_NIT',
+ADD COLUMN nombre_cliente_oc VARCHAR(255) NOT NULL DEFAULT 'DEFAULT_NOMBRE_CLIENTE',
+ADD COLUMN cantidad_oc INT,
+ADD COLUMN nombre_referencia_oc VARCHAR(255),
+ADD COLUMN numero_referencia_oc VARCHAR(50),
+ADD COLUMN archivo_path_oc TEXT NOT NULL DEFAULT 'DEFAULT_PATH',
+ADD COLUMN hora_registro_oc TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN usuario_id_oc INT NOT NULL DEFAULT 1; -- Usamos 1 como valor predeterminado para el usuario, ajusta según el caso.
+
+
+CREATE TABLE ordenes_compras (
+    id SERIAL PRIMARY KEY,
+    nrodcto_oc VARCHAR(50) NOT NULL DEFAULT 'DEFAULT_NRODCTO',
+    bruto_oc DECIMAL(15, 2),
+    ivabruto_oc DECIMAL(15, 2),
+    nit_oc VARCHAR(20) NOT NULL DEFAULT 'DEFAULT_NIT',
+    nombre_cliente_oc VARCHAR(255) NOT NULL DEFAULT 'DEFAULT_NOMBRE_CLIENTE',
+    cantidad_oc INT,
+    nombre_referencia_oc VARCHAR(255),
+    numero_referencia_oc VARCHAR(50),
+    archivo_path_oc TEXT NOT NULL DEFAULT 'DEFAULT_PATH',
+    hora_registro_oc TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    usuario_id_oc INT NOT NULL DEFAULT 1
+);
+
